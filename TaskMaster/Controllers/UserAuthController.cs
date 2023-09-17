79892ca844045f46d5fed703e1555d538fa654d9
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-17 20:57:49 +0800

    Using asp.net core mvc instead of vue and api

---
+++ TaskMaster/Controllers/UserAuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMaster.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
