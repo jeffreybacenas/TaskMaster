f816446f05c00ba58fc2ed897f0627b0021587d3
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-20 20:19:59 +0800

    Adding Forget Password Ui

--- TaskMaster/Controllers/UserAuthController.cs
+++ TaskMaster/Controllers/UserAuthController.cs
@@ -13,5 +13,10 @@ namespace TaskMaster.Controllers
         {
             return View();
         }
+
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
     }
 }
