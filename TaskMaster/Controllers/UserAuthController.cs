bb5bc7bae4b32814229529eb66680275c6ee8f72
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-20 20:04:08 +0800

    Adding Registration and Layout for unauthentacated users.

--- TaskMaster/Controllers/UserAuthController.cs
+++ TaskMaster/Controllers/UserAuthController.cs
@@ -8,5 +8,10 @@ namespace TaskMaster.Controllers
         {
             return View();
         }
+
+        public IActionResult Register() 
+        {
+            return View();
+        }
     }
 }
