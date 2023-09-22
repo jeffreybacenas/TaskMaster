148a2d76af63fcecdac755188e7f337c98fd8aaf
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-22 20:14:10 +0800

    Adding App Layout

--- TaskMaster/Program.cs
+++ TaskMaster/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Authentication.Cookies; // Make sure to include this namespace
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure authentication services inside ConfigureServices method
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(options =>
+//    {
+//        options.LoginPath = "/UserAuth/Login"; // Specify the login path
+//        options.AccessDeniedPath = "/UserAuth/Login"; // Specify the access denied path
+//    });
 
 var app = builder.Build();
 
@@ -18,7 +28,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthentication(); // Move this line to use authentication middleware before authorization
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
