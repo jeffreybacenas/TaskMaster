4d6a21bc5e362af63383924cf475020d0db5466f
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-23 23:05:20 +0800

    Adding category initial design.

---
+++ TaskMaster/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMaster.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Categories()
+        {
+            return View();
+        }
+    }
+}
