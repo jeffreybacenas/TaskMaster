3580967727c4b9491292566417b3c5e3b93f2626
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-26 20:59:11 +0800

    Adding project ui initial module

---
+++ TaskMaster/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMaster.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
