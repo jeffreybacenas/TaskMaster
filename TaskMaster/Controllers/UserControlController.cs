42aa60f6f039d20e04397bc6f0740c572ad9f39b
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-27 16:26:31 +0800

    Adding User Control index

---
+++ TaskMaster/Controllers/UserControlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMaster.Controllers
+{
+    public class UserControlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
