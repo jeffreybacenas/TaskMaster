34c52c4f646357c60435159a8a182a2fea2ee167
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-27 21:01:27 +0800

    Adding profile initial ui

---
+++ TaskMaster/Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMaster.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
