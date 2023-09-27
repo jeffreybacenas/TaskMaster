ea12a39499bac5ecdd8bd694c40fae693d19a426
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-27 20:50:01 +0800

    Adding initial ui for permission

--- TaskMaster/Controllers/UserControlController.cs
+++ TaskMaster/Controllers/UserControlController.cs
@@ -13,5 +13,10 @@ namespace TaskMaster.Controllers
         {
             return View();
         }
+
+        public IActionResult Permission() 
+        {
+            return View();
+        }
     }
 }
